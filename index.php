44f881abefed48bb4df33a65d1cf326d1952a4e2
asfawe <aronia5137>
2022-02-26 22:08:29 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <ul></ul>
+    </ol>
+</body>
+</html>
---
+++ string.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<body>
+<h1>문자열</h1>
+<?php
+echo 'Hello word';
+?>
+<h1>.을 이용해서 결합</h1>
+<?php
+echo 'Hello '.'word';
+?>
+<h1>String length function</h1>
+<?php
+echo strlen('Hello World');
+?>
+</body>
+</html>
+
+
